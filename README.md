ec981e747eb8d9c415075952df65a7b8d24ddbb3
Lee Rogers <lrogersgm@gmail.com>
2021-04-24 11:47:19 +0100

    Refactored to be able to redraw grid of same dimensions with but user defined number of cells.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 1. Etch-a-sketch assignment for the-odin-project. Initial setup.
 2. Implemented grid population and 'sketching' listener functionality.
 3. Implemented 'clear-sketch' functionality.
+4. Refactored to work with user defined number of cells within grid of same dimensions.
--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,41 @@
-function populateGrid(x) {
-    for (let i = 0; i < x; i++) {
+function populateGrid(size) {
+    for (let i = 0; i < size; i++) {
         const gridSquare = document.createElement("div");
         gridSquare.classList.add("gridSquare");
-        gridSquare.setAttribute("id", "gridSquare"+i);
+        gridSquare.setAttribute("id", "gridSquare"+size);
         gridContainer.appendChild(gridSquare);
+    }
+    gridSqaures = document.querySelectorAll('.gridSquare');
+}
+
+function removeOldGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
     }
 }
 
-const resetButton = document.querySelector("#resetButton");
-const gridContainer = document.querySelector("#gridContainer");
+function clearSketch() {
+    gridSqaures.forEach((div) => {
+        div.classList.remove('gridSquareEntered');
+    });
+}
 
-populateGrid(256);
+function addSketchListener() {
+    gridSqaures.forEach((div) => {
+        div.addEventListener('mouseover', () => {
+            div.classList.add("gridSquareEntered");
+        });
+    });
+}
 
-const gridSqaures = document.querySelectorAll('.gridSquare');
+const resetButton = document.querySelector('#resetButton');
+const newSizeButton = document.querySelector('#newSizeButton');
+const gridContainer = document.getElementById('gridContainer');
+
+let gridSqaures = document.querySelectorAll('.gridSquare');
+let size = 256;
+
+populateGrid(size);
 
 gridSqaures.forEach((div) => {
     div.addEventListener('mouseover', () => {
@@ -20,9 +43,17 @@ gridSqaures.forEach((div) => {
     });
 });
 
-resetButton.addEventListener("click", () => {
-    gridSqaures.forEach((div) => {
-        div.classList.remove("gridSquareEntered");
-    });
+resetButton.addEventListener('click', clearSketch);
+
+newSizeButton.addEventListener('click', () => {
+    let newSize = prompt("How many squares per side?");
+    document.documentElement.style.setProperty("--rowNum", newSize);
+    document.documentElement.style.setProperty("--colNum", newSize);
+    size = newSize * newSize;
+    console.log(size);
+    removeOldGrid();
+    populateGrid(size);
+    addSketchListener();
 });
+
 
